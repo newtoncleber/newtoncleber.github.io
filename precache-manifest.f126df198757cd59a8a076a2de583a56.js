dfaa9cf61dced1e7eb099a70681171be336a091b
newtoncleber <newtoncleber@gmail.com>
2020-01-18 17:23:07 -0300

    new favicon set

---
+++ precache-manifest.f126df198757cd59a8a076a2de583a56.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd1c160c77d90b93c35dfd182a0b1e8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "44ba6863ff3ebcf1b353",
+    "url": "./static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d5ae1452d80a181cb12b",
+    "url": "./static/css/main.4d4c8e26.chunk.css"
+  },
+  {
+    "revision": "5c995374f70ad07bbcc4",
+    "url": "./static/js/10.97dad7b5.chunk.js"
+  },
+  {
+    "revision": "e204128e711aa6cda379",
+    "url": "./static/js/11.ad9aff4c.chunk.js"
+  },
+  {
+    "revision": "44ba6863ff3ebcf1b353",
+    "url": "./static/js/2.c96f39c5.chunk.js"
+  },
+  {
+    "revision": "54f6e2d0ef339619d0a432be7ac0ab75",
+    "url": "./static/js/2.c96f39c5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ee7e28b04cef08a94fc",
+    "url": "./static/js/3.6df37505.chunk.js"
+  },
+  {
+    "revision": "316a2df9f9de28e6fceb",
+    "url": "./static/js/4.dfe1e419.chunk.js"
+  },
+  {
+    "revision": "4413cb5c6d857cb62ae0",
+    "url": "./static/js/5.cce1c18e.chunk.js"
+  },
+  {
+    "revision": "74e58b94f2af0ae4caab",
+    "url": "./static/js/6.a840d2ba.chunk.js"
+  },
+  {
+    "revision": "1702115706a8a230aeea",
+    "url": "./static/js/7.3a5fd565.chunk.js"
+  },
+  {
+    "revision": "c3b6593ef387f7d8c607",
+    "url": "./static/js/8.ac991401.chunk.js"
+  },
+  {
+    "revision": "d3dc4c742d55fefeeb83",
+    "url": "./static/js/9.8f0045f7.chunk.js"
+  },
+  {
+    "revision": "d5ae1452d80a181cb12b",
+    "url": "./static/js/main.3d373257.chunk.js"
+  },
+  {
+    "revision": "fde9bad09cedaab80fc2",
+    "url": "./static/js/runtime-main.3c754baf.js"
+  },
+  {
+    "revision": "7e3f1e68e60261202291b2c339ff85ee",
+    "url": "./static/media/shoppingcart.7e3f1e68.svg"
+  },
+  {
+    "revision": "339d67765af18fbdbae3222e08b257dc",
+    "url": "./static/media/usercircle.339d6776.svg"
+  }
+]);
